11387288807d9eda863e4ee5b50b1d2d50af0f38
Masahiro Wada <argon.argon.argon@gmail.com>
2020-09-24 19:50:46 +0900

    Add offline support

--- src/osic_pulmonary_fibrosis_progression/lungmask.py
+++ src/osic_pulmonary_fibrosis_progression/lungmask.py
@@ -1,8 +1,19 @@
+import os
+from pathlib import Path
+
 from lungmask import mask
 import SimpleITK as sitk
 import numpy as np
 
+_LUNGMASK_WEIGHTS_ROOT = Path(
+    os.environ.get("LUNGMASK_WEIGHTS_ROOT", "../input/my-osic2020-data")
+)
+_R231_PTH_PATH = _LUNGMASK_WEIGHTS_ROOT / "unet_r231-d5d2fc3d.pth"
+
+mask.model_urls[("unet", "R231")] = (str(_R231_PTH_PATH), 3)
+
+
 def lung_segmentation(img: np.ndarray) -> np.ndarray:
     input_image = sitk.GetImageFromArray(img)
     input_image.SetDirection(np.eye(3).ravel())
-    return mask.apply(input_image) 
+    return mask.apply(input_image)
