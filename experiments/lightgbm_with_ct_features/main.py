6655cce3c7192bf2f115355e9b030c4e57e3ec7c
Masahiro Wada <argon.argon.argon@gmail.com>
2020-10-01 08:18:18 +0900

    Add config support

--- experiments/lightgbm_with_ct_features/main.py
+++ experiments/lightgbm_with_ct_features/main.py
@@ -22,66 +22,101 @@ import numpy as np
 import torchtoolbox.transform as transforms
 
 # %%
-from osic_pulmonary_fibrosis_progression import io, datasource, lightgbm, dataset, task, config, metric, transforms as my_transforms, dcm, lungmask
+from osic_pulmonary_fibrosis_progression import (
+    io,
+    datasource,
+    lightgbm,
+    dataset,
+    task,
+    config,
+    metric,
+    transforms as my_transforms,
+    dcm,
+    lungmask,
+)
 
 # %%
-train_transform = transforms.Compose([
-    my_transforms.DropSlice(),
-    my_transforms.LungMask(),
-    my_transforms.DescribeVolume()
-])
+train_transform = transforms.Compose(
+    [
+        my_transforms.DropSlice(),
+        my_transforms.LungMask(),
+        my_transforms.DescribeVolume(),
+    ]
+)
 
 # %%
-conf = config.Config()
+conf = config.get_config()
 
 # %%
-all_source, test_source = io.load_osic_pulmonary_fibrosis_progression_csv(use_pseudo_baselines=False, ignore_bad_ct=True)
+all_source, test_source = io.load_osic_pulmonary_fibrosis_progression_csv(
+    use_pseudo_baselines=conf.use_pseudo_baselines, ignore_bad_ct=conf.ignore_bad_ct
+)
 
 # %%
 train_source, val_source = datasource.train_validate_split(all_source)
 train_dataset = dataset.CTDataset(train_source, transforms=train_transform)
 
 # %%
-models, oof_fvc = task.ct_features.train_all_folds(conf, all_source, 4, transforms=train_transform)
+models, oof_fvc = task.ct_features.train_all_folds(
+    conf, all_source, conf.n_fold, transforms=train_transform
+)
 
 # %%
-predictions_fvc = task.ct_features.infer_with_all_folds(conf, test_source, models, transforms=train_transform)
+predictions_fvc = task.ct_features.infer_with_all_folds(
+    conf, test_source, models, transforms=train_transform
+)
 
 # %%
 task.ct_features.show_feature_importance(models, train_dataset.features)
 
 # %%
-all_source.df['FVC_pred'] = oof_fvc
-test_source.df['FVC_pred'] = predictions_fvc
+all_source.df["FVC_pred"] = oof_fvc
+test_source.df["FVC_pred"] = predictions_fvc
 
 # %%
-score = metric.calc_laplace_log_likelihood(all_source.df["FVC"].values, np.array([100] * len(all_source)), all_source.df["FVC_pred"].values)
+score = metric.calc_laplace_log_likelihood(
+    all_source.df["FVC"].values,
+    np.array([100] * len(all_source)),
+    all_source.df["FVC_pred"].values,
+)
 print(score)
 
 # %%
-confidence = metric.calc_best_confidence(all_source.df["FVC"].values, all_source.df["FVC_pred"].values)
+confidence = metric.calc_best_confidence(
+    all_source.df["FVC"].values, all_source.df["FVC_pred"].values
+)
 
 # %%
 # optimized score
-all_source.df['Confidence'] = confidence
-score = metric.calc_laplace_log_likelihood(all_source.df["FVC"].values, all_source.df['Confidence'].values, all_source.df["FVC_pred"].values)
+all_source.df["Confidence"] = confidence
+score = metric.calc_laplace_log_likelihood(
+    all_source.df["FVC"].values,
+    all_source.df["Confidence"].values,
+    all_source.df["FVC_pred"].values,
+)
 print(score)
 
 # %%
-models, oof_confidence = task.ct_features.train_all_folds(conf, all_source, 4,  target="Confidence")
+models, oof_confidence = task.ct_features.train_all_folds(
+    conf, all_source, 4, target="Confidence"
+)
 
 # %%
-predictions_confidence= task.ct_features.infer_with_all_folds(conf, test_source, models)
+predictions_confidence = task.ct_features.infer_with_all_folds(
+    conf, test_source, models
+)
 
 # %%
 all_source.df["Confidence"] = oof_confidence
-score = metric.calc_laplace_log_likelihood(all_source.df["FVC"].values, all_source.df["Confidence"].values, all_source.df["FVC_pred"].values)
+score = metric.calc_laplace_log_likelihood(
+    all_source.df["FVC"].values,
+    all_source.df["Confidence"].values,
+    all_source.df["FVC_pred"].values,
+)
 print(score)
 
 # %%
-test_source.df['Confidence'] = predictions_confidence
+test_source.df["Confidence"] = predictions_confidence
 
 # %%
 io.save_result(test_source)
-
-# %%
--- src/osic_pulmonary_fibrosis_progression/config.py
+++ src/osic_pulmonary_fibrosis_progression/config.py
@@ -2,11 +2,24 @@ import os
 from dataclasses import dataclass
 from typing import Optional
 
+from distutils.util import strtobool
+
 
 @dataclass
 class Config:
-    pass
+    n_fold: int
+    use_pseudo_baselines: bool
+    ignore_bad_ct: bool
 
 
 def get_config() -> Config:
-    return Config()
+    n_fold = int(os.environ.get("KAGGLE_N_FOLD", 4))
+    use_pseudo_baselines = strtobool(
+        os.environ.get("KAGGLE_USE_PSEUDO_BASELINES", "False")
+    )
+    ignore_bad_ct = strtobool(os.environ.get("KAGGLE_IGNORE_BAD_CT", "True"))
+    return Config(
+        n_fold=n_fold,
+        use_pseudo_baselines=use_pseudo_baselines,
+        ignore_bad_ct=ignore_bad_ct,
+    )
